53e80fe3d5332740ac4eed9d8b4be712f4bb849d
MichalStempkowski <michulix@gmail.com>
2014-10-22 21:47:01 +0200

    modification check

--- ReversiApp/tests/test_sanity.py
+++ ReversiApp/tests/test_sanity.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 
 
 # Create your tests here.
-class SanityCheck(TestCase):
+class TestSanity(TestCase):
     def setUp(self):
         pass
 
     def when_running_tests__should_pass_this_test(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
