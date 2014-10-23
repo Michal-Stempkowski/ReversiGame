acee1ac527574660ecdae29786638f737769ccb3
MichalStempkowski <michulix@gmail.com>
2014-10-23 10:08:03 +0200

    starting position added

--- ReversiApp/gameboard/game_board.py
+++ ReversiApp/gameboard/game_board.py
@@ -1,17 +1,36 @@
-from functools import reduce
 from ReversiApp.fluent_flow.functional import generator_len
 
-__author__ = 'Michał'
+
+class Piece(object):
+    def __eq__(self, other):
+        return self.__class__ is other.__class__
+
+
+class WhitePiece(Piece):
+    pass
+
+
+class BlackPiece(Piece):
+    pass
+
+
+class NoPiece(Piece):
+    pass
 
 
 class GameBoard(object):
     def __init__(self):
-        self.fields = [[None for _ in range(8)] for _ in range(8)]
+        self.fields = [[NoPiece() for _ in range(8)] for _ in range(8)]
 
     @staticmethod
     def count_fields_in_row(field_type, row):
-        return generator_len(filter(lambda field: field is field_type, row))
+        return generator_len(filter(lambda field: field == field_type, row))
 
     def count_empty_fields(self):
-        return reduce(lambda count, row:
-                      count + self.count_fields_in_row(None, row), self.fields, 0)
+        return sum(self.count_fields_in_row(NoPiece(), row) for row in self.fields)
+
+    def add_new_game_starting_pieces(self):
+        self.fields[3][3] = WhitePiece()
+        self.fields[3][4] = BlackPiece()
+        self.fields[4][4] = WhitePiece()
+        self.fields[4][3] = BlackPiece()
--- ReversiApp/tests/gameboard/test_when_creating_game_board.py
+++ ReversiApp/tests/gameboard/test_when_creating_game_board.py
@@ -1,16 +1,34 @@
 from unittest import TestCase
 
-from ReversiApp.gameboard.game_board import GameBoard
+from ReversiApp.gameboard.game_board import GameBoard, WhitePiece, BlackPiece
 
 
 class WhenCreatingGameBoard(TestCase):
     def setUp(self):
         pass
 
-    def test_should_be_able_to_create_empty_board(self):
-        game_board = GameBoard()
-
     def test_newly_created_board_should_have_64_empty_fields(self):
         game_board = GameBoard()
 
         self.assertEquals(64, game_board.count_empty_fields())
+
+    def test_board_with_five_pieces_should_have_59_empty_fields(self):
+        game_board = GameBoard()
+        game_board.fields[0][0] = WhitePiece()
+        game_board.fields[1][0] = WhitePiece()
+        game_board.fields[0][3] = WhitePiece()
+        game_board.fields[0][6] = BlackPiece()
+        game_board.fields[4][4] = BlackPiece()
+
+        self.assertEquals(59, game_board.count_empty_fields())
+
+    def test_after_initializing_board__there_should_be_2_white_and_2_black_pieces(self):
+        game_board = GameBoard()
+
+        game_board.add_new_game_starting_pieces()
+
+        self.assertEquals(60, game_board.count_empty_fields())
+        self.assertEquals(WhitePiece(), game_board.fields[3][3])
+        self.assertEquals(BlackPiece(), game_board.fields[3][4])
+        self.assertEquals(WhitePiece(), game_board.fields[4][4])
+        self.assertEquals(BlackPiece(), game_board.fields[4][3])
