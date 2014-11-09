7df0200d3450df8d1cb3484a8dcae67dcfdbd528
MichalStempkowski <michulix@gmail.com>
2014-11-09 19:20:50 +0100

    GameState Command pattern adopted

--- ReversiApp/core/game_logic.py
+++ ReversiApp/core/game_logic.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from ReversiApp.core.game_board import GameBoard
 
 
@@ -9,43 +10,77 @@ class Game(object):
     def get_current_game_state(self):
         return self.game_state
 
+    def perform_action(self, action):
+        action(self)
+        # action_result = action(self)
+        # if action_result.requires_game_state_change():
+        #     self.game_state = action_result.get_game_state_change()
 
-class UnknownGameStateException(Exception):
+
+class UnreachableGameStateException(Exception):
     pass
 
 
 class GameState(object):
+    def __init__(self, reachable_states):
+        self.reachable_states = {x.name() for x in reachable_states}
+
     def __repr__(self):
         return self.__class__.__name__
 
     def __eq__(self, other):
         return self.__class__ is other.__class__
 
-    def __getattr__(self, item, *args):
-        raise UnknownGameStateException
+    @classmethod
+    def name(cls):
+        return cls.__name__
 
-    def is_transition_possible(self, transition):
-        return transition in self.__dir__()
+    def is_state_reachable(self, state):
+        return state.name() in self.reachable_states
 
 
 class GameStateNew(GameState):
-    def quit(self, game_logic):
-        game_logic.game_state = GameStateDead()
-
-    def initialize(self, game_logic):
-        game_logic.game_board = GameBoard()
-        game_logic.game_board.add_new_game_starting_pieces()
-        game_logic.game_state = GameStateInitialized()
+    def __init__(self):
+        super().__init__([GameStateDead, GameStateInitialized])
 
 
 class GameStateDead(GameState):
-    pass
+    def __init__(self):
+        super().__init__([])
 
 
 class GameStateInitialized(GameState):
-    def start_game(self, game_logic):
-        game_logic.game_state = GameStateBlackTurn()
+    def __init__(self):
+        super().__init__([GameStateBlackTurn])
 
 
 class GameStateBlackTurn(GameState):
-    pass
+    def __init__(self):
+        super().__init__([])
+
+
+class Action(object):
+    @staticmethod
+    def raise_if_state_unreachable(game_logic, state):
+        if not game_logic.game_state.is_state_reachable(state):
+            raise UnreachableGameStateException
+
+
+class BlackTurnAction(Action):
+    def __call__(self, game_logic):
+        self.raise_if_state_unreachable(game_logic, GameStateBlackTurn())
+        game_logic.game_state = GameStateBlackTurn()
+
+
+class QuitAction(Action):
+    def __call__(self, game_logic):
+        self.raise_if_state_unreachable(game_logic, GameStateDead())
+        game_logic.game_state = GameStateDead()
+
+
+class InitializeAction(Action):
+    def __call__(self, game_logic):
+        self.raise_if_state_unreachable(game_logic, GameStateInitialized())
+        game_logic.game_board = GameBoard()
+        game_logic.game_board.add_new_game_starting_pieces()
+        game_logic.game_state = GameStateInitialized()
--- ReversiApp/tests/test_game_logic.py
+++ ReversiApp/tests/test_game_logic.py
@@ -19,35 +19,31 @@ class WhenInNewState(TestCase):
         self.game_logic = Game()
 
     def test_should_raise_exception_on_unregistered_state_change(self):
-        with self.assertRaises(UnknownGameStateException):
-            self.game_logic.game_state.some_unknown_state()
+        with self.assertRaises(UnreachableGameStateException):
+            self.game_logic.perform_action(BlackTurnAction())
 
     def test_should_move_to_dead_state_after_quit_call(self):
-        try:
-            self.game_logic.game_state.quit(self.game_logic)
-        except UnknownGameStateException:
-            self.fail("UnknownGameStateException thrown for known method!")
-
+        self.game_logic.perform_action(QuitAction())
         self.assertEquals(GameStateDead(), self.game_logic.get_current_game_state())
 
-    def test_should_be_able_to_check_if_transition_is_available(self):
-        self.assertFalse(self.game_logic.game_state.is_transition_possible('unknown'))
-        self.assertTrue(self.game_logic.game_state.is_transition_possible('quit'))
+    def test_should_be_able_to_check_if_state_is_reachable(self):
+        self.assertTrue(self.game_logic.game_state.is_state_reachable(GameStateDead()))
+        self.assertFalse(self.game_logic.game_state.is_state_reachable(GameStateBlackTurn()))
 
     def test_should_move_to_initialized_state_after_initialize_call(self):
-        self.game_logic.game_state.initialize(self.game_logic)
+        self.game_logic.perform_action(InitializeAction())
         self.assertEquals(GameStateInitialized(), self.game_logic.get_current_game_state())
 
 
 class WhenInInitializedState(TestCase):
     def setUp(self):
         self.game_logic = Game()
-        self.game_logic.game_state.initialize(self.game_logic)
+        self.game_logic.perform_action(InitializeAction())
 
     def test_game_board_should_be_ready_to_play(self):
         self.assertNotEquals(None, self.game_logic.game_board)
         self.assertTrue(self.game_logic.game_board.is_ready_for_new_game())
 
     def test_should_move_to_black_turn_state_after_start_game_call(self):
-        self.game_logic.game_state.start_game(self.game_logic)
+        self.game_logic.perform_action(BlackTurnAction())
         self.assertEquals(GameStateBlackTurn(), self.game_logic.get_current_game_state())
